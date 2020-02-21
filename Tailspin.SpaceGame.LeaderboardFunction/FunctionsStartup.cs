2d44ecabcbb080b55b892c31b4e7ee7638aeebae
Ed Kaim <ed@sharplogic.com>
2020-02-21 13:51:22 -0800

    Add project files.

---
+++ Tailspin.SpaceGame.LeaderboardFunction/FunctionsStartup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(TailSpin.SpaceGame.LeaderboardFunction.Startup))]
+namespace TailSpin.SpaceGame.LeaderboardFunction
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<IDocumentDBRepository<Score>>(new LocalDocumentDBRepository<Score>(@"SampleData/scores.json"));
+            builder.Services.AddSingleton<IDocumentDBRepository<Profile>>(new LocalDocumentDBRepository<Profile>(@"SampleData/profiles.json"));
+
+        }
+    }
+}
---
+++ Tailspin.SpaceGame.LeaderboardFunction/LeaderboardFunction.cs
@@ -0,0 +1,104 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using TailSpin.SpaceGame.LeaderboardFunction;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tailspin.SpaceGame.LeaderboardFunction
+{
+    public class LeaderboardFunction
+    {
+        // High score repository.
+        private readonly IDocumentDBRepository<Score> _scoreRepository;
+
+        // User profile repository.
+        private readonly IDocumentDBRepository<Profile> _profileRespository;
+
+        public LeaderboardFunction(
+            IDocumentDBRepository<Score> scoreRepository,
+            IDocumentDBRepository<Profile> profileRespository)
+
+        {
+            this._scoreRepository = scoreRepository;
+            this._profileRespository = profileRespository;
+        }
+
+        [FunctionName("LeaderboardFunction")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Leaderboard function processed a request.");
+
+            // Grab parameters from query string.
+            string mode = req.Query["mode"];
+            string region = req.Query["region"];
+
+            int page = 0;
+            int.TryParse(req.Query["page"], out page);
+
+            int pageSize;
+            if (int.TryParse(req.Query["pageSize"], out pageSize))
+            {
+                pageSize = Math.Max(Math.Min(pageSize, 50), 1);
+            }
+            else
+            {
+                pageSize = 10;
+            }
+
+            // Create the baseline response.
+            var leaderboardResponse = new LeaderboardResponse()
+            {
+                Page = page,
+                PageSize = pageSize,
+                SelectedMode = mode,
+                SelectedRegion = region
+            };
+
+            // Form the query predicate.
+            // This expression selects all scores that match the provided game 
+            // mode and region (map).
+            // Select the score if the game mode or region is empty.
+            Expression<Func<Score, bool>> queryPredicate = score =>
+                (string.IsNullOrEmpty(mode) || score.GameMode == mode) &&
+                (string.IsNullOrEmpty(region) || score.GameRegion == region);
+
+            // Fetch the total number of results in the background.
+            var countItemsTask = this._scoreRepository.CountItemsAsync(queryPredicate);
+
+            // Fetch the scores that match the current filter.
+            IEnumerable<Score> scores = await this._scoreRepository.GetItemsAsync(
+                queryPredicate, // the predicate defined above
+                score => score.HighScore, // sort descending by high score
+                page - 1, // subtract 1 to make the query 0-based
+                pageSize
+              );
+
+            // Wait for the total count.
+            leaderboardResponse.TotalResults = await countItemsTask;
+
+            // Fetch the user profile for each score.
+            // This creates a list that's parallel with the scores collection.
+            var profiles = new List<Task<Profile>>();
+            foreach (var score in scores)
+            {
+                profiles.Add(this._profileRespository.GetItemAsync(score.ProfileId));
+            }
+            Task<Profile>.WaitAll(profiles.ToArray());
+
+            // Combine each score with its profile.
+            leaderboardResponse.Scores = scores.Zip(profiles, (score, profile) => new ScoreProfile { Score = score, Profile = profile.Result });
+
+            return (ActionResult)new OkObjectResult(leaderboardResponse);
+        }
+    }
+}
