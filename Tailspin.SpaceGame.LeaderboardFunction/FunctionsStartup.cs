c66cc7e1f73f1cd230f437bd3e5d6ade31ced1f6
Ed Kaim <ed@sharplogic.com>
2020-03-01 20:57:56 -0800

    Testing deploy fix

--- Tailspin.SpaceGame.LeaderboardFunction/FunctionsStartup.cs
+++ Tailspin.SpaceGame.LeaderboardFunction/FunctionsStartup.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Reflection;
 
 [assembly: FunctionsStartup(typeof(TailSpin.SpaceGame.LeaderboardFunction.Startup))]
 namespace TailSpin.SpaceGame.LeaderboardFunction
@@ -8,8 +11,14 @@ namespace TailSpin.SpaceGame.LeaderboardFunction
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            builder.Services.AddSingleton<IDocumentDBRepository<Score>>(new LocalDocumentDBRepository<Score>(@"SampleData/scores.json"));
-            builder.Services.AddSingleton<IDocumentDBRepository<Profile>>(new LocalDocumentDBRepository<Profile>(@"SampleData/profiles.json"));
+            // https://github.com/Azure/azure-functions-host/issues/4517#issuecomment-497940595
+            var actual_root = Environment.GetEnvironmentVariable("AzureWebJobsScriptRoot")  // local_root
+                    ?? (Environment.GetEnvironmentVariable("HOME") == null
+                        ? Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+                        : $"{Environment.GetEnvironmentVariable("HOME")}/site/wwwroot"); // azure_root
+            
+            builder.Services.AddSingleton<IDocumentDBRepository<Score>>(new LocalDocumentDBRepository<Score>(Path.Combine(actual_root, @"SampleData/scores.json")));
+            builder.Services.AddSingleton<IDocumentDBRepository<Profile>>(new LocalDocumentDBRepository<Profile>(Path.Combine(actual_root, @"SampleData/profiles.json")));
 
         }
     }
