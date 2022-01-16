f8138dbe98b43e4e2432474e20252b4b3e077105
Hirofumi Horikawa <hori.kawa.shima.tool+github@gmail.com>
2022-01-16 14:49:13 +0900

    Update LeaderboardFunctionClient.cs

--- Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardFunctionClient.cs
+++ Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardFunctionClient.cs
@@ -21,7 +21,7 @@ namespace TailSpin.SpaceGame.Web
             using (WebClient webClient = new WebClient())
             {
                 string json = await webClient.DownloadStringTaskAsync($"{this._functionUrl}?page={page}&pageSize={pageSize}&mode={mode}&region={region}");
-                return JsonSerializer.Deserialize<LeaderboardResponse>(json);
+                return JsonSerializer.Deserialize<LeaderboardResponse>(json, new JsonSerializerOptions { IncludeFields = true });
             }
         }
     }
